d6663460dbbf4e9e21de0c34ba4cab2ebed90c17
Evan Tzanis <evan.tzanis@credit-vision.com>
2020-02-18 15:20:27 +0000

    https://www.hackerrank.com/challenges/the-love-letter-mystery

---
+++ src/LoveLetter.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class LoveLetter {
+
+
+    private static boolean checkPalindrome(String variable) {
+
+
+        StringBuffer rev = new StringBuffer(variable).reverse();
+
+        String strRev = rev.toString();
+
+        if (variable.equalsIgnoreCase(strRev))
+            return true;
+        else
+            return false;
+
+    }
+
+
+    static int theLoveLetterMystery(String s) {
+
+        int counter=0;
+        StringBuilder buildPalindrome = new StringBuilder(s);
+
+        for (int i=0; i<(s.length()/2); i++) {
+
+            if(buildPalindrome.charAt(i)!=buildPalindrome.charAt(s.length()-i-1)) {
+
+                buildPalindrome.setCharAt(s.length() - i - 1, buildPalindrome.charAt(i));
+                counter+=Math.abs((int)s.charAt(i) - (int)s.charAt(s.length()-i-1));
+            }
+
+
+        }
+
+        return counter;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(theLoveLetterMystery("abcd"));
+    }
+
+
+    }
